93a0bbc535a24f97865c3e8b24c7761f64c4fae8
TDTer <80698862+TDTer@users.noreply.github.com>
2024-07-12 18:44:39 +0700

    Update gameplay

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -102,45 +102,45 @@ public class PlayerController : MonoBehaviour
 
 
     //Check Platform
-    private RuntimePlatform platform
-    {
-        get
-        {
-#if UNITY_ANDROID
-            return RuntimePlatform.Android;
-#elif UNITY_IOS 
-            return RuntimePlatform.IPhonePlayer;
-#elif UNITY_STANDALONE_OSX
-             return RuntimePlatform.OSXPlayer;
-#elif UNITY_STANDALONE_WIN
-            return RuntimePlatform.WindowsPlayer;
-#endif
-        }
-    }
+    //     private RuntimePlatform platform
+    //     {
+    //         get
+    //         {
+    // #if UNITY_ANDROID
+    //             return RuntimePlatform.Android;
+    // #elif UNITY_IOS 
+    //             return RuntimePlatform.IPhonePlayer;
+    // #elif UNITY_STANDALONE_OSX
+    //              return RuntimePlatform.OSXPlayer;
+    // #elif UNITY_STANDALONE_WIN
+    //             return RuntimePlatform.WindowsPlayer;
+    // #endif
+    //         }
+    //     }
 
-    public bool isTouchInterface
-    {
-        get
-        {
-#if UNITY_EDITOR
-            // Game is being played in the editor and the selected BuildTarget is either Android or iOS
-            if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android ||
-        EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
-            {
-                return true;
-            }
-#endif
-            // Game is being played on an Android or iOS device
-            if (platform == RuntimePlatform.Android ||
-            platform == RuntimePlatform.IPhonePlayer)
-            {
-                return true;
-            }
-            // Game is being played on something other then an Android or iOS device
-            else
-            {
-                return false;
-            }
-        }
-    }
+    //     public bool isTouchInterface
+    //     {
+    //         get
+    //         {
+    // #if UNITY_EDITOR
+    //             // Game is being played in the editor and the selected BuildTarget is either Android or iOS
+    //             if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android ||
+    //         EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
+    //             {
+    //                 return true;
+    //             }
+    // #endif
+    //             // Game is being played on an Android or iOS device
+    //             if (platform == RuntimePlatform.Android ||
+    //             platform == RuntimePlatform.IPhonePlayer)
+    //             {
+    //                 return true;
+    //             }
+    //             // Game is being played on something other then an Android or iOS device
+    //             else
+    //             {
+    //                 return false;
+    //             }
+    //         }
+    //     }
 }
